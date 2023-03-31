93fe581f4903f7b46819461726a4093b86889ab3
abhi040498 <abhishekpandey04041997@gmail.com>
2023-03-31 15:15:08 +0530

    Airdrop sol to custom address

---
+++ SolanaBeginner/AirdropToCustomAdd.js
@@ -0,0 +1,15 @@
+
+
+// const fromAirDropSignature = await connection.requestAirdrop(
+//     new PublicKey(myWallet.publicKey),
+//     2 * LAMPORTS_PER_SOL
+// );
+
+// Entering the public key recieved by the user
+// Declaring the variable.
+var pubKeyByUser;
+// Assigning the value in publickey function
+const fromAirDropSignature = await connection.requestAirdrop(
+    new PublicKey(pubKeyByUser),
+    2 * LAMPORTS_PER_SOL
+);
